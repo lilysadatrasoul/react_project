b3b0ac9a84ad0b380fc0a6bbd08767773b03f70f
Lily Sadat <l.sadatrasoul@gmail.com>
2022-02-25 23:18:18 +0330

    first init

--- src/App.js
+++ src/App.js
@@ -1,25 +1,51 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css"
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+     <div dir="rtl">
+       <div className="header">
+            <img src="1.png" alt="پاساژ" className="logophoto"/>
+            <input type="search" id="search" name="search" placeholder="کالای مورد نظر خود را جستجو کنید"/>
+            <input type="submit" value="جستجو" id="submit"/>
+       </div>
+        <div class="navbar">
+           <div className="dropdown">
+               <button className="dropbtn">کالای دیجیتال 
+                <i className="fa fa-caret-down"></i>
+               </button>
+               <div className="dropdown-content">
+                  <a href="#">Link 1</a>
+                  <a href="#">Link 2</a>
+                  <a href="#">Link 3</a>
+               </div>
+            </div>
+           <div className="dropdown">
+               <button className="dropbtn">مد و پوشاک 
+                <i className="fa fa-caret-down"></i>
+               </button>
+               <div className="dropdown-content">
+                  <a href="#">Link 1</a>
+                  <a href="#">Link 2</a>
+                  <a href="#">Link 3</a>
+               </div>
+            </div>
+           <div className="dropdown">
+               <button className="dropbtn">آرایشی و بهداشتی 
+                <i className="fa fa-caret-down"></i>
+               </button>
+               <div className="dropdown-content">
+                  <a href="#">Link 1</a>
+                  <a href="#">Link 2</a>
+                  <a href="#">Link 3</a>
+               </div>
+            </div>
+        </div>
+
+        <div className="photocenter">
+         
+        </div>
+     </div>
+    );
 }
 
 export default App;
