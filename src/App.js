8b1946fa24358054db4cb1d733b6e787596bc3f9
Lily Sadat <l.sadatrasoul@gmail.com>
2022-02-28 20:00:35 +0330

    fixing init page

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import "./App.css"
+import logo from "./assets/images/1.png"
+import product1 from "./assets/images/1.jpg"
+import product2 from "./assets/images/2.jpg"
+import product3 from "./assets/images/3.jpg"
+import product4 from "./assets/images/4.jpg"
 
 function App() {
   return (
      <div dir="rtl">
        <div className="header">
-            <img src="1.png" alt="پاساژ" className="logophoto"/>
+            <img src={logo} alt="پاساژ" className="logophoto"/>
             <input type="search" id="search" name="search" placeholder="کالای مورد نظر خود را جستجو کنید"/>
             <input type="submit" value="جستجو" id="submit"/>
        </div>
@@ -41,7 +46,55 @@ function App() {
             </div>
         </div>
 
-        <div className="photocenter">
+        <div className="products">
+           <div className="productName">
+              <img src={product1} alt="" />
+              <div className="product_title">
+                 <p>گوشی موبایل شیائومی  </p>
+                 <p>
+                 10,750,000 تومان
+                 </p>
+              </div>
+              <div>
+                 <button className="btn" > اضافه کردن به سبد خرید</button>
+              </div>
+           </div>
+           <div className="productName">
+              <img src={product2} alt="" />
+              <div className="product_title">
+                 <p>گوشی موبایل سامسونگ  </p>
+                 <p>
+                 5,550,000 تومان
+                 </p>
+              </div>
+              <div>
+                 <button className="btn" > اضافه کردن به سبد خرید</button>
+              </div>
+           </div>
+           <div className="productName">
+              <img src={product3} alt="" />
+              <div className="product_title">
+                 <p>گوشی موبایل هاویی   </p>
+                 <p>
+                 13,750,000 تومان
+                 </p>
+              </div>
+              <div>
+                 <button className="btn" > اضافه کردن به سبد خرید</button>
+              </div>
+           </div>
+           <div className="productName">
+              <img src={product4} alt="" />
+              <div className="product_title">
+                 <p>گوشی موبایل اچ تی سی   </p>
+                 <p>
+                 9,375,000 تومان
+                 </p>
+              </div>
+              <div>
+                 <button className="btn" > اضافه کردن به سبد خرید</button>
+              </div>
+           </div>
          
         </div>
      </div>
--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom';//virtual dom react
 
 import App from './App';
 
